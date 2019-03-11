fc7f8b5547a7959cf5416250420f0d54a0475a1c
Sato <s_ychm@yahoo.co.jp>
2019-03-12 08:24:21 +0900

    for文にリストを挿入

--- 2s_list.py
+++ 2s_list.py
@@ -2,6 +2,16 @@ users = [['Bob', 11],
          ['Tom', 34],
          ['Ken', 65]]
 
-print(f'{users[0][0]}さんは{users[0][1]}歳です')
+for user in users:
+    print(f'{user[0]}さんは{user[1]}歳です')
+#
+# for user in users:
+#     print(user[0])
 
-print(f'{users[1][0]}さんは{users[1][1]}歳です')
+# print(f'{users[0][0]}さんは{users[0][1]}歳です')
+#
+# print(f'{users[1][0]}さんは{users[1][1]}歳です')
+
+# for user_name in users:
+#     for user_age in users:
+#         print(f'{user_name[0]}さんは{user_age[1]}歳です')
